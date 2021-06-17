c611e00fcbf16d19e1153388e0dbfc2cdbebf214
Martina Zapletalová <zapletalova.m@mz-mee.eu>
2021-06-17 12:03:47 +0200

    Script pro tooltip

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,19 @@ function toggleControls() {
 
   document.getElementsByClassName('section-discover__video-overlay')[0].style.visibility='hidden';
 }
+
+jQuery(document).ready(function() {
+    jQuery('.info').mouseover(function(){
+        var tooltip = jQuery(this).children('.info-tooltip');
+        var height = jQuery(tooltip).outerHeight() + 10;
+        var left = jQuery(this).siblings('.info-text').outerWidth();
+        jQuery(tooltip).css({'top': "-" + height + 'px', 'left': "-" + left + 'px'});
+    });
+
+    jQuery('.info').click(function(){
+        var tooltip = jQuery(this).children('.info-tooltip');
+        var height = jQuery(tooltip).outerHeight() + 10;
+        var left = jQuery(this).siblings('.info-text').outerWidth();
+        jQuery(tooltip).css({'top': "-" + height + 'px', 'left': "-" + left + 'px'});
+    });
+});
