c9a4aaa6c609f5591b2384d0187dea496672dbc0
Karel Tropp <koder@kareltropp.cz>
2021-06-23 09:48:18 +0200

    uprava header link under btn

--- template-homepage.php
+++ template-homepage.php
@@ -42,7 +42,7 @@ get_header();
                         if(get_sub_field('hero_button')):
                             $link = get_sub_field('hero_button');?>
                             <a href="<?php echo esc_url( $link['url'] ); ?>" class="btn btn-primary btn-primary--noshadow"><?php echo esc_attr( $link['title'] ); ?></a>
-                            <a href="https://app.livestorm.co/roivenue/roivenue-pre-recorded-demo?type=detailed" target="_blank" class="btn-link">Watch pre-recorded demo</a>
+                            <a href="https://app.livestorm.co/roivenue/roivenue-pre-recorded-demo?type=detailed" target="_blank" class="btn-link">Or watch 3-minute demo now</a>
                         <?php 
                         endif;?>
                     </div>
