47070a4029bccb11e0291f782e41b2d0f8ce5743
Karel Tropp <koder@kareltropp.cz>
2021-07-19 09:37:07 +0200

    upravy footer

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 <?php endif;
 
 if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
-
+    <?php /*
     <footer id="main-footer">
         <?php get_sidebar( 'footer' ); ?>
 
@@ -59,7 +59,7 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
         </div>
     </footer> <!-- #main-footer -->
     </div> <!-- #et-main-area -->
-
+*/ ?>
 <?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
 
 </div> <!-- #page-container -->
@@ -69,64 +69,19 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
     <div class="container">
         <div class="footer__row">
             <div class="footer__col">
-                <a href="" class="footer__logo">
-                    <img src="https://roivenue.com/wp-content/uploads/2021/07/roivenue-logo-horizontal.png">
-                </a>
-                <div class="footer__desc">
-                    <strong>Made in Prague with <img src="https://roivenue.com/wp-content/uploads/2021/07/heart.png"><br>by marketers for marketers</strong><br><br>
-                    <a href="tel:00420774140220">+420 774 140 220</a><br>
-                    <a href="mailto:hello@roivenue.com">hello@roivenue.com</a>
-                </div>
-                <div class="footer__socials footer__socials--desktop">
-                    <a href="https://www.linkedin.com/company/roivenue/">
-                        <img src="https://roivenue.com/wp-content/uploads/2021/07/linkedin.png">
-                    </a>
-                    <a href="https://www.facebook.com/roivenue/">
-                        <img src="https://roivenue.com/wp-content/uploads/2021/07/fb.png">
-                    </a>
-                    <a href="https://twitter.com/@roivenue">
-                        <img src="https://roivenue.com/wp-content/uploads/2021/07/twitter.png">
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCzrOMbxErWasRv95maadenQ">
-                        <img src="https://roivenue.com/wp-content/uploads/2021/07/youtube.png">
-                    </a>
-                    <a href="https://www.instagram.com/roivenue/?hl=en">
-                        <img src="https://roivenue.com/wp-content/uploads/2021/07/instagram.png">
-                    </a>
-                </div>
-                <div class="footer__socials footer__socials--mobile">
-                    <a href="https://www.linkedin.com/company/roivenue/">
-                        <img src="https://roivenue.com/wp-content/uploads/2019/03/footer-linkedin.png">
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCzrOMbxErWasRv95maadenQ">
-                        <img src="https://roivenue.com/wp-content/uploads/2019/03/footer-youtube.png">
-                    </a>
-                    <a href="https://www.instagram.com/roivenue/?hl=en">
-                        <img src="https://roivenue.com/wp-content/uploads/2019/03/footer-instagram.png">
-                    </a>
-                    <a href="https://twitter.com/@roivenue">
-                        <img src="https://roivenue.com/wp-content/uploads/2019/03/footer-twitter.png">
-                    </a>
-                    <a href="https://www.facebook.com/roivenue/">
-                        <img src="https://roivenue.com/wp-content/uploads/2019/03/footer-facebook.png">
-                    </a>
-                </div>
+                <?php dynamic_sidebar('sidebar-2'); ?>
             </div>
             <div class="footer__col">
-                <div class="footer__badges">
-                    <img src="https://roivenue.com/wp-content/uploads/2021/07/1.png"><br>
-                    <img src="https://roivenue.com/wp-content/uploads/2021/07/2.png"><br>
-                    <img src="https://roivenue.com/wp-content/uploads/2021/07/3.png">
-                </div>
+                <?php dynamic_sidebar('sidebar-3'); ?>
             </div>
             <div class="footer__col">
                 <div class="footer__menu">
                     <div class="footer__title">Products</div>
                     <?=
-                        wp_nav_menu([
-                            'theme_location' => 'footer_menu1',
-                            'container' => false,
-                        ]);
+                    wp_nav_menu([
+                        'theme_location' => 'footer_menu1',
+                        'container' => false,
+                    ]);
                     ?>
                 </div>
                 <div class="footer__menu">
